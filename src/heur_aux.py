864659bbb26a9f7453e8b970a6d4cfa4aa3f5923
Matej Mojzes <you@example.com>
2026-03-04 12:58:59 +0100

    third lecture

---
+++ src/heur_aux.py
@@ -0,0 +1,89 @@
+import numpy as np
+import numpy.typing as npt
+from objfun import ObjFun
+
+def is_integer(a: npt.NDArray) -> bool:
+    """
+    Tests if `a` is integer
+    """
+    dt = a.dtype
+    return dt == np.int16 or dt == np.int32 or dt == np.int64
+
+
+class Correction:
+
+    """
+    Baseline mutation correction strategy - "sticks" the solution to domain boundaries
+    """
+
+    def __init__(self, of: ObjFun) -> None:
+        self.of = of
+
+    def correct(self, x: npt.NDArray) -> npt.NDArray:
+        return np.minimum(np.maximum(x, self.of.a), self.of.b)
+
+
+class MirrorCorrection(Correction):
+    """
+    Mutation correction via mirroring
+    """
+
+    def __init__(self, of: ObjFun) -> None:
+        Correction.__init__(self, of)
+
+    def correct(self, x: npt.NDArray) -> npt.NDArray:
+        n = np.size(x)
+        d = self.of.b - self.of.a
+        for k in range(n):
+            if d[k] == 0:
+                x[k] = self.of.a[k]
+            else:
+                de = np.mod(x[k] - self.of.a[k], 2*d[k])
+                de = np.amin([de, 2*d[k] - de])
+                x[k] = self.of.a[k] + de
+        return x
+
+
+class ExtensionCorrection(Correction):
+    """
+    Mutation correction via periodic domain extension
+    """
+
+    def __init__(self, of: ObjFun) -> None:
+        Correction.__init__(self, of)
+
+    def correct(self, x: npt.NDArray) -> npt.NDArray:
+        d = self.of.b - self.of.a
+        x = self.of.a + np.mod(x - self.of.a, d + (1 if is_integer(x) else 0))
+        return x
+
+
+class Mutation:
+
+    """
+    Generic mutation super-class
+    """
+
+    def __init__(self, correction: Correction) -> None:
+        self.correction = correction
+
+
+class CauchyMutation(Mutation):
+
+    """
+    Cauchy mutation
+    """
+
+    def __init__(self, r: float, correction: Correction) -> None:
+        Mutation.__init__(self, correction)
+        self.r = r
+
+    def mutate(self, x: npt.NDArray, rng: np.random.Generator) -> npt.NDArray:
+        n = np.size(x)
+        u = rng.uniform(low=0.0, high=1.0, size=n)
+        r = self.r
+        x_new = x + r * np.tan(np.pi * (u - 1 / 2))
+        if is_integer(x):
+            x_new = np.array(np.round(x_new), dtype=int)  # optional rounding
+        x_new_corrected = self.correction.correct(x_new)
+        return x_new_corrected
